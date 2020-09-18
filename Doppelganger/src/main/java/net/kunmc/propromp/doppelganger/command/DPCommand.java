af119adca252e6d545c1ca3fb7da045e272c86e5
Propromp <proprompx2@gmail.com>
2020-09-18 21:29:34 +0900

    @aに対応させた

--- Doppelganger/src/main/java/net/kunmc/propromp/doppelganger/command/DPCommand.java
+++ Doppelganger/src/main/java/net/kunmc/propromp/doppelganger/command/DPCommand.java
@@ -34,12 +34,20 @@ public class DPCommand {
 
 	private static void exec(Player player, Command cmd, String commandLabel, String[] args) {
 		player.sendMessage("変更中");
-		DoppelAPI.doppelAll(args[1]);
+		if(args[1].equals("@a")) {
+			DoppelAPI.doppelAll(args[2]);
+		} else {
+			DoppelAPI.doppel(UUID.fromString(args[1]),args[2]);
+		}
 		player.sendMessage("[DoppelGanger]処理が完了しました。スキン、ニックネームの適用には時間が掛かる可能性があります。");
 	}
 	private static void clear(Player player,Command cmd,String commandLabel,String[] args) {
 		player.sendMessage("変更中");
-		DoppelAPI.clearAll();
+		if(args[1].equals("@a")) {
+			DoppelAPI.clearAll();
+		} else {
+			DoppelAPI.clear(UUID.fromString(args[1]));
+		}
 		player.sendMessage("[DoppelGanger]処理が完了しました。スキン、ニックネームの適用には時間が掛かる可能性があります。");
 	}
 }
